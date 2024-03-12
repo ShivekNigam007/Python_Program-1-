c1a9bb4c781d5e5b019b644ad194d5e6103ff5b8
ShivekNigam007 <163147080+ShivekNigam007@users.noreply.github.com>
2024-03-12 19:24:32 +0530

    Add files via upload

---
+++ First.py
@@ -0,0 +1,36 @@
+# print("Hello World")
+# print("My name is Shivek Nigam.","\nMy age is 21.")
+# print(12)
+# print(10+10)
+# name = "Shivek"//Strings
+# name1 = "Shivek"
+# name2 = 'Shivam'
+# name3 = '''Satyam'''
+# age = -21
+# age2 = age
+# price = -98.56
+# print("My name is :",name)
+# print("My age is :",age)
+# print("And its price is:",price)
+# print(age2)
+# print(type(name))
+# print(type(age))
+# print(type(price))
+# print(name1)
+# print(name2)
+# print(name3)
+# age = 21
+# old = False
+# a = None
+
+# print(type(age))
+# print(type(old))
+# print(type(a))
+
+# print(input("Enter a value of a:"))
+# print(input("Enter a value of b:"))
+
+a = 5
+b = 5
+sum = a+b
+print(sum)
